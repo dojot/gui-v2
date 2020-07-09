0f1472cc445f01e0f03b025e869019502627be00
Welliton Souza <wellitonaz@yahoo.com.br>
2020-07-08 21:04:25 -0300

    Login

--- src/js/views/login/login.jsx
+++ src/js/views/login/login.jsx
@@ -1,20 +1,50 @@
-import React, { Component, useEffect, useState } from 'react'
-
-const login = (props) => {       
-    return (
-        <div className="login">
-            <h1>Login</h1>
-            <form>
-                <p>Usuário</p>
-                <input type='text' name="user"/>
-                <p>Senha</p>
-                <input type="password" name="password"/>
-                <p>
-                <input type="submit" value="Login"/>
-                </p>
-            </form> 
-        </div>
+import React from 'react';
+ 
+import { makeStyles,} from '@material-ui/core/styles';
+import { Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+ 
+ 
+ 
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin:'5px',
+      },
+ 
+    grid:{
+        width: '250px',
+        margin: '50px', 
+       
+    },
+ 
+    root: {
+      '& > *': {
+        margin: theme.spacing(2),
+        width: '25ch',
+      },
+    },
+  }));
+ 
+  export default function login() {
+    const classes = useStyles();
+    return ( 
+                <Grid container justify="center" style={{ flex: '1' }} >
+                    <Card className={classes.grid} >
+                            <form className={classes.root} autoComplete="off" >
+                                <TextField id="user" label="Login" variant="outlined"  size="large"/>
+                                <TextField id="password" label="Password" type="password" 
+                                autoComplete="current-password" variant="outlined" size="large"/>       
+                                <FormControlLabel control={
+                                <Checkbox color="primary"/>
+                                } label="Remember me" />
+                                <Button variant="outlined" color="secondary" size="large" className={classes.margin} >Login</Button>
+                            </form>
+                            
+                    </Card>
+                </Grid>
+       
     );
 }
+ 
+ 
 
-export default login;
