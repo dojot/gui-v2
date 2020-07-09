49e9987001d15918bdfd9303f09dc6562b539643
Welliton Souza <wellitonaz@yahoo.com.br>
2020-07-09 12:12:00 -0300

    Login

--- src/js/views/login/login.jsx
+++ src/js/views/login/login.jsx
@@ -1,48 +1,40 @@
 import React from 'react';
  
 import { makeStyles,} from '@material-ui/core/styles';
-import { Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+import { Grid, TextField, Button } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
  
  
  
 const useStyles = makeStyles((theme) => ({
     margin: {
-        margin:'5px',
+        marginTop:16,
+        marginBottom:8,
       },
  
     grid:{
-        width: '250px',
-        margin: '50px', 
+      width: '250px',
+      margin: '50px', 
+      padding: 16,
        
-    },
- 
-    root: {
-      '& > *': {
-        margin: theme.spacing(2),
-        width: '25ch',
-      },
-    },
+    },   
+   
   }));
  
-  export default function login() {
+  export default ()=> {
     const classes = useStyles();
     return ( 
-                <Grid container justify="center" style={{ flex: '1' }} >
-                    <Card className={classes.grid} >
-                            <form className={classes.root} autoComplete="off" >
-                                <TextField id="user" label="Login" variant="outlined"  size="large"/>
-                                <TextField id="password" label="Password" type="password" 
-                                autoComplete="current-password" variant="outlined" size="large"/>       
-                                <FormControlLabel control={
-                                <Checkbox color="primary"/>
-                                } label="Remember me" />
-                                <Button variant="outlined" color="secondary" size="large" className={classes.margin} >Login</Button>
-                            </form>
-                            
-                    </Card>
-                </Grid>
-       
+        <Grid container justify="center" style={{ flex: '1' }} >
+            <Card className={classes.grid} >
+                    <form className={classes.root} autoComplete="off"  >
+                        <TextField id="user" label="Login" variant="outlined"  fullWidth margin="normal" size="medium" />
+                        <TextField id="password" label="Password" type="password" 
+                            autoComplete="current-password" variant="outlined" fullWidth margin="normal" size="medium" />
+                        <Button variant="outlined" color="secondary" size="medium" fullWidth >Login</Button>
+                    </form>
+                    
+            </Card>
+        </Grid>       
     );
 }
  
