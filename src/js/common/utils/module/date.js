54f1f783b3ea439a1a17c8f425b30c2eb5c1a1a3
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2020-07-24 15:20:57 -0300

    feat - timezone adjust

--- src/js/common/utils/module/date.js
+++ src/js/common/utils/module/date.js
@@ -1,15 +1,9 @@
 import moment from 'moment';
 
-moment.locale('pt-br');
-
 export const formatDate = tickItem => {
   return moment(tickItem).format('HH:mm:ss');
 };
 
 export const formatToISO = date => {
-  return date
-    ? moment(date)
-        .toISOString(true)
-        .replace('-03:00', 'Z')
-    : '';
+  return date ? moment(date).toISOString() : '';
 };
