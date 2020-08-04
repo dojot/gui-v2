61871f6424bbd4bf15f4a294e1d8f937413a8a09
Luis Felipe Furlan <lfcruz@cpqd.com.br>
2020-08-04 17:10:18 -0300

    fix: Unnecessary CSS Class Removed

--- src/js/common/components/Table/SimpleTable/SimpleTable.jsx
+++ src/js/common/components/Table/SimpleTable/SimpleTable.jsx
@@ -7,27 +7,36 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 
 import useStyles from './style';
 
 const CustomizedTables = ({ columns, rows }) => {
-  const { table, head, root } = useStyles();
+  const { head, root } = useStyles();
 
   return (
     <TableContainer classes={{ root }}>
-      <Table classes={{ table }} stickyHeader aria-label="customized table">
+      <Table stickyHeader aria-label="customized table">
         <TableHead>
           <TableRow>
             {columns.map(column => {
-              return <TableCell classes={{ head }}>{column.label}</TableCell>;
+              return (
+                <TableCell key={column.dataKey} classes={{ head }}>
+                  {column.label}
+                </TableCell>
+              );
             })}
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map(row => (
-            <TableRow key={row.label}>
+            <TableRow key={`${row.label}_${uuidv4()}`}>
               {columns.map(column => {
-                return <TableCell>{row[column.dataKey] || '-'}</TableCell>;
+                return (
+                  <TableCell key={`${column.dataKey}_${uuidv4()}`}>
+                    {row[column.dataKey] || '-'}
+                  </TableCell>
+                );
               })}
             </TableRow>
           ))}
