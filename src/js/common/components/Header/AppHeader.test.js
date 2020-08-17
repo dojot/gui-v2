179a23fb8bbe8da8bac281b9ac501b49feec0541
wellitonSouza <wellitonaz@yahoo.com.br>
2020-08-17 09:40:19 -0300

    Lint

--- src/js/common/components/Header/AppHeader.test.js
+++ src/js/common/components/Header/AppHeader.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import { render, fireEvent } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
 
 import AppHeader from './AppHeader';
@@ -16,31 +14,26 @@ describe('AppHeader', () => {
             title={title}
             handleClick = {mockedHandleClick}
           />);
-        
         expect(container).toBeInTheDocument();
     });
 
 
-    it('should be able to click on open button', ()=>{       
-
-        const { container, getByTestId } = render(<AppHeader
-            isOpen = {isOpen}
-            title={title}
-            handleClick = {mockedHandleClick}
-        />);
-        expect(container).toBeInTheDocument();
-        fireEvent.click(getByTestId('butonOpen'));
-        expect(mockedHandleClick).toHaveBeenCalled();
-    });
-
-    it('should be able to click on close button', ()=>{       
-
+    it('should be able to click on open button', ()=>{
         const { getByTestId } = render(<AppHeader
             isOpen = {isOpen}
             title={title}
             handleClick = {mockedHandleClick}
         />);
+        fireEvent.click(getByTestId('butonOpen'));
+        expect(mockedHandleClick).toHaveBeenCalled();
+    });
 
+    it('should be able to click on close button', ()=>{       
+        const { getByTestId } = render(<AppHeader
+            isOpen = {isOpen}
+            title={title}
+            handleClick = {mockedHandleClick}
+        />);
         fireEvent.click(getByTestId('butonNotOpen'));
         expect(mockedHandleClick).toHaveBeenCalled();
     });
