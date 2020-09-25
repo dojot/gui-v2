9f7c29ecc34d49402959ff785d839174f0bd7b7d
Luis Felipe Furlan Cruz <lfcruz@cpqd.com.br>
2020-09-25 15:50:50 -0300

    imp: two digit precision

--- src/js/common/utils/module/number.js
+++ src/js/common/utils/module/number.js
@@ -1,4 +1,4 @@
 import numeral from 'numeral';
 
-export const formatNumber = (tickItem, type = '0.0a') =>
+export const formatNumber = (tickItem, type = '0.00a') =>
   numeral(tickItem).format(type);
