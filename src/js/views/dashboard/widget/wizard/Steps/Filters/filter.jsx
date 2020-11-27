3367595a82810120900e2dcb08b8fc764bce4bec
Luis Felipe Furlan Cruz <lfcruz@cpqd.com.br>
2020-11-27 13:13:09 -0300

    Widget map with static value

--- src/js/views/dashboard/widget/wizard/Steps/Filters/filter.jsx
+++ src/js/views/dashboard/widget/wizard/Steps/Filters/filter.jsx
@@ -121,12 +121,12 @@ const Filters = props => {
 
   const handleChangeFixedValue = useCallback(event => {
     const { value } = event.target;
-    setData(state => ({ ...state, fixedValue: value }));
+    setData(state => ({ ...state, fixedValue: value.replace(/\D/gim, '') }));
   }, []);
 
   const handleChangeDynamicValue = useCallback(event => {
     const { value } = event.target;
-    setData(state => ({ ...state, dynamicValue: value }));
+    setData(state => ({ ...state, dynamicValue: value.replace(/\D/gim, '') }));
   }, []);
 
   useEffect(() => {
@@ -216,6 +216,8 @@ const Filters = props => {
                       type='number'
                       onChange={handleChangeFixedValue}
                       label='Nº Registros'
+                      label={t('filters.records')}
+                      inputProps={{ min: 1, max: 9999, step: 1 }}
                       disabled={data.filterType !== '0'}
                     />
                   </FormControl>
@@ -264,6 +266,7 @@ const Filters = props => {
                       onChange={handleChangeDynamicValue}
                       label='Nº Registros'
                       disabled={data.filterType !== '1'}
+                      inputProps={{ min: 1, max: 9999, step: 1 }}
                     />
                   </FormControl>
                 </div>
