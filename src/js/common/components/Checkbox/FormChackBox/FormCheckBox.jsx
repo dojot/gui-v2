cba1502bb3603a68a64fd2f037bd608f679455ea
Luis Felipe Furlan <lfcruz@cpqd.com.br>
2021-03-12 17:12:48 -0300

    fix: Fixed selectable disabled checkbox

--- src/js/common/components/Checkbox/FormChackBox/FormCheckBox.jsx
+++ src/js/common/components/Checkbox/FormChackBox/FormCheckBox.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Checkbox from '@material-ui/core/Checkbox';
 
-export const FormCheckBox = ({ input: { onChange, checked } }) => {
+export const FormCheckBox = ({ input: { onChange, checked }, disabled }) => {
   return (
     <Checkbox
       edge='start'
+      disabled={disabled}
       checked={checked}
       tabIndex={-1}
       disableRipple
