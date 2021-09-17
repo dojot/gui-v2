17fef3a5940a47394e95740efdd0a2bc0e1fbf4a
Luan Eduardo da Costa <luan1346@gmail.com>
2021-09-17 15:28:25 -0300

    fix: Show only the dashboard menu item

--- src/js/common/menu/menuItems.js
+++ src/js/common/menu/menuItems.js
@@ -10,21 +10,21 @@ import {
 
 export const MENU_ITEMS = [
   {
-    visible: true,
+    visible: false,
     i18n: 'devices',
     label: 'Dispositivos',
     path: '/devices',
     icon: DevicesOther,
   },
   {
-    visible: true,
+    visible: false,
     i18n: 'templates',
     label: 'Modelos',
     path: '/templates',
     icon: FilterNone,
   },
   {
-    visible: true,
+    visible: false,
     i18n: 'flows',
     label: 'Fluxos',
     path: '/flow',
@@ -37,14 +37,14 @@ export const MENU_ITEMS = [
     icon: NotificationImportant,
   },
   {
-    visible: true,
+    visible: false,
     i18n: 'users',
     label: 'Usuários',
     path: '/users',
     icon: People,
   },
   {
-    visible: true,
+    visible: false,
     i18n: 'profiles',
     label: 'Perfis',
     path: '/profiles',
