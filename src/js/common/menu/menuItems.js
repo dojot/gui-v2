fbe663a64d4ae62791bd6a9d2aaab0924c31809d
Luan Eduardo da Costa <luan1346@gmail.com>
2021-10-05 12:35:41 -0300

    Nx Feature - Create Home Page (#83)
    * feat: Create home page
    
    * Update home page card list

--- src/js/common/menu/menuItems.js
+++ src/js/common/menu/menuItems.js
@@ -3,6 +3,7 @@ import {
   DeviceHub,
   DevicesOther,
   FilterNone,
+  Home,
   NotificationImportant,
   People,
   SupervisedUserCircle,
@@ -49,6 +50,13 @@ export const MENU_ITEMS = [
     label: 'Perfis',
     path: '/profiles',
     icon: SupervisedUserCircle,
+  },
+  {
+    visible: true,
+    i18n: 'home',
+    label: 'home',
+    path: '/home',
+    icon: Home,
   },
   {
     visible: true,
