094ece2d3b4e7015e0648afcdf94543ab0ada32f
Luan Eduardo da Costa <luan1346@gmail.com>
2021-10-05 15:50:59 -0300

    Create the user info component layout

--- src/js/views/stateComponents/UserInfo/style.js
+++ src/js/views/stateComponents/UserInfo/style.js
@@ -5,15 +5,20 @@ export const useStyles = makeStyles(theme => ({
     display: 'flex',
   },
   list: {
-    width: '100%',
-    maxWidth: 360,
+    minWidth: 160,
     backgroundColor: theme.palette.background.paper,
+    borderRadius: '5px',
+  },
+  listItemIcon: {
+    minWidth: 'auto',
+    marginRight: theme.spacing(2),
   },
   divider: {
     backgroundColor: 'rgba(255, 255, 255, 0.8)',
     margin: '0 15px',
   },
   button: {
-    textTransform: 'none',
+    textTransform: 'capitalize',
   },
+  paper: {},
 }));
