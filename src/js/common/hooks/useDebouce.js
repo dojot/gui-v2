ad5b58e62e76ef6ff4c1c419f027b6e6553fd033
Luan Eduardo da Costa <luan1346@gmail.com>
2021-10-06 12:22:34 -0300

    Add JSDoc to useDebouce hook

--- src/js/common/hooks/useDebouce.js
+++ src/js/common/hooks/useDebouce.js
@@ -1,5 +1,14 @@
 import { useRef } from 'react';
 
+/**
+ * Creates a generic debouce function.
+ *
+ * @param {object} options Hook options
+ * @param {function} options.startCallback Function called when the debouce started.
+ * @param {function} options.stopCallback Function called when the debouce stops.
+ * @param {function} options.delay Delay in milliseconds. Default is 600.
+ * @returns {function} Debounce function. Calls startCallback, clears the timer, and calls the stopCallback after a delay.
+ */
 export const useDebounce = ({ stopCallback, startCallback, delay = 600 } = {}) => {
   const timer = useRef(0);
 
