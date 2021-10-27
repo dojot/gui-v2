4df4d7b20958ffc69013f20ca9a4e2692533a291
LeonamMoura <leo99.music@gmail.com>
2021-10-27 14:04:39 -0300

    refactor: PR corrections

---
+++ src/js/views/CertificationAuthorities/layout/Cards.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { Box, Grid, Typography } from '@material-ui/core';
+import { VerifiedUserOutlined } from '@material-ui/icons';
+import PropTypes from 'prop-types';
+
+import DataCard from '../../../common/components/Cards/DataCard';
+import { useCardsStyles } from './style';
+
+const Cards = ({ page, certificationAuthorities, rowsPerPage, handleSetCaOptionsMenu }) => {
+  const classes = useCardsStyles();
+
+  return (
+    <Box padding={2}>
+      <Grid spacing={2} container>
+        {certificationAuthorities
+          .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          .map(certificationAuthority => {
+            const handleShowOptionsMenu = e => {
+              e.stopPropagation();
+              handleSetCaOptionsMenu({
+                anchorElement: e.target,
+                certificationAuthority,
+              });
+            };
+
+            return (
+              <Grid key={certificationAuthority.id} xs={12} sm={6} md={4} xl={3} item>
+                <DataCard
+                  className={classes.deviceCard}
+                  headerIcon={<VerifiedUserOutlined className={classes.deviceCardIcon} />}
+                  headerTitle={
+                    <Typography className={classes.deviceCardTitle}>
+                      {certificationAuthority.name}
+                    </Typography>
+                  }
+                  onOptionsClick={handleShowOptionsMenu}
+                />
+              </Grid>
+            );
+          })}
+      </Grid>
+    </Box>
+  );
+};
+
+Cards.propTypes = {
+  page: PropTypes.number,
+  certificationAuthorities: PropTypes.array,
+  rowsPerPage: PropTypes.number,
+  handleSetCaOptionsMenu: PropTypes.func,
+};
+
+Cards.defaultProps = {
+  page: 0,
+  certificationAuthorities: [],
+  rowsPerPage: 0,
+  handleSetCaOptionsMenu: null,
+};
+
+export default Cards;
