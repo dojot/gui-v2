960686510f313a65726376e67fdac3bf64b5eb67
LeonamMoura <leo99.music@gmail.com>
2021-11-11 13:11:56 -0300

    create certificates commom service

---
+++ src/js/common/services/certificates.service.js
@@ -0,0 +1,62 @@
+import { protectAPI } from 'APIs';
+
+export const getCertificatesList = (page, filter) => {
+  return protectAPI({
+    query: `
+      query getCertificates($page: PageInput, $filter: FilterDeviceInput) {
+        getCertificates(page: $page, filter: $filter) {
+          totalPages
+          currentPage
+          certificates {
+            id
+            label
+            attrs{
+              label
+              valueType
+              isDynamic
+              staticValue
+            }
+          }
+        }
+      }
+    `,
+    variables: JSON.stringify({
+      page,
+      filter,
+    }),
+  });
+};
+
+export const deleteCertificate = certificateId => {
+  return protectAPI({
+    query: `
+      mutation deleteCertificate($certificateId: String!) {
+        deleteCertificate(certificateId: $certificateId) {
+          id
+        }
+      }
+    `,
+    variables: JSON.stringify({
+      certificateId,
+    }),
+  });
+};
+
+export const deleteMultipleCertificates = certificatesIdArray => {
+  return protectAPI({
+    query: `
+      mutation deleteMultipleCertificates($certificatesIdArray: [String]!) {
+        deleteMultipleCertificates(certificatesIdArray: $certificatesIdArray) {
+          id
+        }
+      }
+    `,
+    variables: JSON.stringify({
+      certificatesIdArray,
+    }),
+  });
+};
+
+export const getCertificatesHistoryParsed = filter => {
+  return protectAPI(filter);
+};
