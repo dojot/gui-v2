ac3035b9435a1f9d9f03acbbd41c69da4cc3530a
LeonamMoura <leo99.music@gmail.com>
2021-11-12 12:25:01 -0300

    refactor cards component

--- src/js/views/Certificates/layout/Cards.jsx
+++ src/js/views/Certificates/layout/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Box, Grid, IconButton, Tooltip, Typography } from '@material-ui/core';
-import { Check, Close, CertificatesOther, Star, StarBorderOutlined } from '@material-ui/icons';
+import { Box, Grid, Typography, Tooltip } from '@material-ui/core';
+import { VerifiedUserOutlined } from '@material-ui/icons';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
@@ -14,7 +14,6 @@ const Cards = ({
   certificates,
   rowsPerPage,
   handleClickCertificate,
-  handleFavoriteCertificate,
   handleSetCertificateOptionsMenu,
 }) => {
   const { t } = useTranslation(['certificates', 'common']);
@@ -26,15 +25,8 @@ const Cards = ({
         {certificates
           .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
           .map(certificate => {
-            const lastUpdate = certificate.updated || certificate.created;
-
             const handleSeeCertificateDetails = () => {
               handleClickCertificate(certificate);
-            };
-
-            const handleFavoriteThisCertificate = e => {
-              e.stopPropagation();
-              handleFavoriteCertificate(certificate);
             };
 
             const handleShowOptionsMenu = e => {
@@ -51,67 +43,36 @@ const Cards = ({
                   className={classes.card}
                   onClick={handleSeeCertificateDetails}
                   onOptionsClick={handleShowOptionsMenu}
-                  headerIcon={<CertificatesOther className={classes.cardIcon} />}
+                  headerIcon={<VerifiedUserOutlined className={classes.cardIcon} />}
                   headerTitle={
                     <Typography className={classes.cardTitle}>{certificate.label}</Typography>
                   }
                   footer={
                     <>
-                      <Tooltip
-                        title={t(
-                          certificate.favorite ? 'removeFromFavoriteTooltip' : 'favoriteTooltip',
-                        )}
-                        placement='top'
-                        arrow
-                      >
-                        <IconButton onClick={handleFavoriteThisCertificate} size='small'>
-                          {certificate.favorite ? (
-                            <Star style={{ color: '#F1B44C' }} />
-                          ) : (
-                            <StarBorderOutlined />
-                          )}
-                        </IconButton>
-                      </Tooltip>
-
-                      <Tooltip
-                        title={t(
-                          certificate.hasCertificate
-                            ? 'hasCertificateTooltip'
-                            : 'noCertificateTooltip',
-                        )}
-                        placement='right'
-                        arrow
-                      >
+                      <Tooltip title={t('lastUpdateTooltip')} placement='right' arrow>
                         <div>
-                          <IconButton size='small' disabled>
-                            {certificate.hasCertificate ? (
-                              <Check color='primary' />
-                            ) : (
-                              <Close color='error' />
+                          <Typography variant='body2'>
+                            {moment(certificate.updated || certificate.created).format(
+                              'DD/MM/YYYY HH:mm:ss',
                             )}
-                          </IconButton>
+                          </Typography>
                         </div>
                       </Tooltip>
                     </>
                   }
                 >
-                  {certificate.attrsLength >= 0 && (
-                    <Box marginBottom={1}>
-                      <Typography variant='body2'>
-                        <strong>{certificate.attrsLength}</strong>
-                      </Typography>
-                      <Typography variant='body2'>{t('cardData.properties')}</Typography>
-                    </Box>
-                  )}
-
-                  {!!lastUpdate && (
-                    <Box>
-                      <Typography variant='body2'>
-                        <strong>{moment(lastUpdate).format('DD/MM/YYYY HH:mm:ss')}</strong>
-                      </Typography>
-                      <Typography variant='body2'>{t('cardData.updated')}</Typography>
-                    </Box>
-                  )}
+                  <Box marginBottom={1}>
+                    <Typography
+                      variant='body2'
+                      className={certificate.deviceId && classes.deviceId}
+                    >
+                      {t(
+                        certificate.deviceId
+                          ? certificate.deviceId
+                          : 'certificates:noAssociatedDeviceTooltip',
+                      )}
+                    </Typography>
+                  </Box>
                 </DataCard>
               </Grid>
             );
@@ -126,7 +87,6 @@ Cards.propTypes = {
   certificates: PropTypes.array,
   rowsPerPage: PropTypes.number,
   handleClickCertificate: PropTypes.func,
-  handleFavoriteCertificate: PropTypes.func,
   handleSetCertificateOptionsMenu: PropTypes.func,
 };
 
@@ -135,7 +95,6 @@ Cards.defaultProps = {
   certificates: [],
   rowsPerPage: 0,
   handleClickCertificate: null,
-  handleFavoriteCertificate: null,
   handleSetCertificateOptionsMenu: null,
 };
 
