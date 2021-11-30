6b4a7823b764781e894a370dcbf984d43bc50a41
Luan Eduardo da Costa <luan1346@gmail.com>
2021-11-30 17:18:42 -0300

    Disable not working card

--- src/js/views/home/View.jsx
+++ src/js/views/home/View.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import { Grid, Typography, Card, CardContent, Box, CardActionArea } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  Box,
+  CardActionArea,
+  Tooltip,
+} from '@material-ui/core';
 import {
   DevicesOther,
   Dashboard,
   FilterNone,
   VerifiedUser,
   ImportExport,
-  Star,
 } from '@material-ui/icons';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router';
@@ -45,11 +52,7 @@ const Home = () => {
       icon: <ImportExport style={{ color: '#50A5F1' }} fontSize='large' />,
       translationKey: 'importExport',
       route: '/import-export',
-    },
-    DEVICE_TEST: {
-      icon: <Star style={{ color: '#F1B44C' }} fontSize='large' />,
-      translationKey: 'deviceTest',
-      route: '/device-test',
+      disabled: true,
     },
   };
 
@@ -58,20 +61,28 @@ const Home = () => {
       <Box sx={{ flexGrow: 1 }} padding={2}>
         <Grid container wrap spacing={4}>
           {Object.entries(HOME_CARDS).map(([key, card]) => {
+            const isDisabled = !!card.disabled;
+
             const handleNavigate = () => {
               if (card.route) history.push(card.route);
             };
 
             return (
               <Grid key={key} xs={12} sm={6} md={3} item>
-                <Card className={classes.card} onClick={handleNavigate}>
-                  <CardActionArea style={{ height: '100%' }}>
-                    <CardContent className={classes.cardContent}>
-                      {card.icon}
-                      <Typography>{t(card.translationKey)}</Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
+                <Tooltip title={t(isDisabled ? 'notAvailableAtTheMoment' : '')} placement='bottom'>
+                  <Card className={`${classes.card} ${isDisabled ? classes.cardDisabled : ''}`}>
+                    <CardActionArea
+                      style={{ height: '100%' }}
+                      disabled={isDisabled}
+                      onClick={handleNavigate}
+                    >
+                      <CardContent className={classes.cardContent}>
+                        {card.icon}
+                        <Typography>{t(card.translationKey)}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Tooltip>
               </Grid>
             );
           })}
