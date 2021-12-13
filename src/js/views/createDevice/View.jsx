7a18eda8ad3061d1036be353e6e249d10bda7efe
Luan Eduardo da Costa <luan1346@gmail.com>
2021-12-13 12:06:56 -0300

    Use template attr static value in the device creation page

--- src/js/views/createDevice/View.jsx
+++ src/js/views/createDevice/View.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import { Box, Grid } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
@@ -114,6 +114,21 @@ const CreateDevice = () => {
       }),
     );
   };
+
+  // Every time a template is selected this useEffect sets the attr static value as default attr value
+  useEffect(() => {
+    Object.values(selectedTemplates).forEach(template => {
+      template.attrs.forEach(attr => {
+        if (attr.type === TEMPLATE_ATTR_TYPES.STATIC.value) {
+          setStaticAttrValues(currentAttrValues => {
+            const actualValue = currentAttrValues[attr.id];
+            const value = actualValue || attr.staticValue || '';
+            return { ...currentAttrValues, [attr.id]: value };
+          });
+        }
+      });
+    });
+  }, [selectedTemplates]);
 
   return (
     <ViewContainer headerTitle={t('title')}>
