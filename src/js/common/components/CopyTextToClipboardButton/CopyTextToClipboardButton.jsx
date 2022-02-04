591050ae8cab25de7a124505eab229fd5f02a06b
LeonamMoura <leo99.music@gmail.com>
2022-02-04 10:15:48 -0300

    adding handling appropriately for async function

--- src/js/common/components/CopyTextToClipboardButton/CopyTextToClipboardButton.jsx
+++ src/js/common/components/CopyTextToClipboardButton/CopyTextToClipboardButton.jsx
@@ -5,17 +5,25 @@ import { Done, FileCopyOutlined } from '@material-ui/icons';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
+import { actions as errorActions } from '../../../redux/modules/errors';
+
 const CopyTextToClipboardButton = ({ textToCopy }) => {
   const [isShowingSuccessTooltip, setIsShowingSuccessTooltip] = useState(false);
 
   const { t } = useTranslation('common');
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(textToCopy);
-    setIsShowingSuccessTooltip(true);
-    setTimeout(() => {
-      setIsShowingSuccessTooltip(false);
-    }, 2000);
+    navigator.clipboard.writeText(textToCopy).then(
+      () => {
+        setIsShowingSuccessTooltip(true);
+        setTimeout(() => {
+          setIsShowingSuccessTooltip(false);
+        }, 2000);
+      },
+      err => {
+        errorActions.addError({ message: err.message, i18nMessage: 'failedToCopy' });
+      },
+    );
   };
 
   return (
