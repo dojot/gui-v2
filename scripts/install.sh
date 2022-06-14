31f586f7f5d635459177722dcdb2f5ebefdd92c4
Luan Eduardo da Costa <luan1346@gmail.com>
2022-06-14 13:11:08 -0300

    Add scripts to improve productivity

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+cd MFE
+
+cd common
+yarn install
+cd ..
+
+cd container
+yarn install
+cd ..
+
+cd dashboard
+yarn install
+cd ..
+
+cd devices
+yarn install
+cd ..
+
+cd home
+yarn install
+cd ..
+
+cd security
+yarn install
+cd ..
+
+cd templates
+yarn install
+cd ..
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+cd MFE
+
+gnome-terminal --tab -e "bash -c 'cd common && yarn start'" --tab -e "bash -c 'cd container && yarn start'" --tab -e "bash -c 'cd dashboard && yarn start'" --tab -e "bash -c 'cd devices && yarn start'" --tab -e "bash -c 'cd home && yarn start'" --tab -e "bash -c 'cd security && yarn start'" --tab -e "bash -c 'cd templates && yarn start'"
