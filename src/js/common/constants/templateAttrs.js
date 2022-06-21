fd71489c32c3714502e06baaeb5b9fda3e9f9f58
LeonamMoura <leo99.music@gmail.com>
2022-06-21 11:18:19 -0300

    Create constants for value_types of attributes

---
+++ src/js/common/constants/templateAttrs.js
@@ -0,0 +1,30 @@
+export const TEMPLATE_ATTR_VALUE_TYPES = Object.freeze({
+  BOOL: {
+    value: 'bool',
+    translation: 'attrs:attrValueType.bool',
+  },
+  GEO_POINT: {
+    value: 'geo:point',
+    translation: 'attrs:attrValueType.geo_point',
+  },
+  FLOAT: {
+    value: 'float',
+    translation: 'attrs:attrValueType.float',
+  },
+  INTEGER: {
+    value: 'integer',
+    translation: 'attrs:attrValueType.integer',
+  },
+  STRING: {
+    value: 'string',
+    translation: 'attrs:attrValueType.string',
+  },
+  OBJECT: {
+    value: 'object',
+    translation: 'attrs:attrValueType.object',
+  },
+  JSON: {
+    value: 'json',
+    translation: 'attrs:attrValueType.json',
+  },
+});
