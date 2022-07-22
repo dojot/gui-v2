9ee793b82a7ce51992ab69c87ab08752516feb62
Luan Eduardo da Costa <luan1346@gmail.com>
2022-07-22 12:35:24 -0300

    Feature - Crete Build Scripts (#197)
    * Create scripts to deal with docker images
    
    * Fix import that makes the build fails

---
+++ scripts/removeImages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo 
+echo "WARNING: This script deletes all MFE docker images."
+echo 
+
+read -p "Are you sure? (y/n): " REPLY
+if [ "$REPLY" = "y" ]; then
+  sudo docker image rm common-nx
+  sudo docker image rm dashboard-nx
+  sudo docker image rm devices-nx
+  sudo docker image rm home-nx
+  sudo docker image rm security-nx
+  sudo docker image rm templates-nx
+else
+  echo 
+  echo "Operation cancelled";
+  echo 
+fi
+
