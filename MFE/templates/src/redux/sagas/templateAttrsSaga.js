a6f8e35edace107056dac4582c79709d4005402a
Luan Eduardo da Costa <luan1346@gmail.com>
2022-08-18 11:37:21 -0300

    Show customer message errors

--- MFE/templates/src/redux/sagas/templateAttrsSaga.js
+++ MFE/templates/src/redux/sagas/templateAttrsSaga.js
@@ -2,6 +2,7 @@ import { put, fork, takeLatest, call } from 'redux-saga/effects';
 import { TemplateAttr } from 'Services';
 import { dispatchEvent } from 'sharedComponents/Hooks';
 import { EVENT } from 'sharedComponents/Constants';
+import { getErrorTranslation } from 'sharedComponents/Utils';
 
 import { actions as loadingActions } from '../modules/loading';
 import { constants } from '../modules/templateAttrs';
@@ -12,13 +13,17 @@ export function* handleDeleteAttr(action) {
     const { templateId, attrId, successCallback } = action.payload;
     yield call(TemplateAttr.deleteTemplateAttrs, templateId, [attrId]);
     if (successCallback) yield call(successCallback);
-    dispatchEvent(EVENT.GLOBAL_TOAST, { duration: 15000, i18nMessage: "deleteAttr", type: "success", });
+    dispatchEvent(EVENT.GLOBAL_TOAST, {
+      duration: 15000,
+      i18nMessage: 'deleteAttr',
+      type: 'success',
+    });
   } catch (e) {
     dispatchEvent(EVENT.GLOBAL_TOAST, {
       duration: 15000,
       message: e.message,
-      i18nMessage: "deleteAttr",
-      type: "error",
+      i18nMessage: 'deleteAttr',
+      type: 'error',
     });
   } finally {
     yield put(loadingActions.removeLoading(constants.DELETE_ATTR));
@@ -31,13 +36,17 @@ export function* handleDeleteMultipleAttrs(action) {
     const { templateId, attrIds, successCallback } = action.payload;
     yield call(TemplateAttr.deleteTemplateAttrs, templateId, attrIds);
     if (successCallback) yield call(successCallback);
-    dispatchEvent(EVENT.GLOBAL_TOAST, { duration: 15000, i18nMessage: "deleteMultipleAttrs", type: "success", });
+    dispatchEvent(EVENT.GLOBAL_TOAST, {
+      duration: 15000,
+      i18nMessage: 'deleteMultipleAttrs',
+      type: 'success',
+    });
   } catch (e) {
     dispatchEvent(EVENT.GLOBAL_TOAST, {
       duration: 15000,
       message: e.message,
-      i18nMessage: "deleteMultipleAttrs",
-      type: "error",
+      i18nMessage: 'deleteMultipleAttrs',
+      type: 'error',
     });
   } finally {
     yield put(loadingActions.removeLoading(constants.DELETE_MULTIPLE_ATTRS));
@@ -50,13 +59,21 @@ export function* handleCreateAttr(action) {
     const { templateId, attr, successCallback } = action.payload;
     yield call(TemplateAttr.createTemplateAttr, templateId, attr);
     if (successCallback) yield call(successCallback);
-    dispatchEvent(EVENT.GLOBAL_TOAST, { duration: 15000, i18nMessage: "createAttr", type: "success", });
-  } catch (e) {
     dispatchEvent(EVENT.GLOBAL_TOAST, {
       duration: 15000,
+      i18nMessage: 'createAttr',
+      type: 'success',
+    });
+  } catch (e) {
+    const i18nMessage = getErrorTranslation(e, 'createAttr', {
+      attrs_template_id_type_label_key: 'attrUnique',
+    });
+
+    dispatchEvent(EVENT.GLOBAL_TOAST, {
+      i18nMessage,
+      type: 'error',
+      duration: 15000,
       message: e.message,
-      i18nMessage: "createAttr",
-      type: "error",
     });
   } finally {
     yield put(loadingActions.removeLoading(constants.CREATE_ATTR));
@@ -69,13 +86,21 @@ export function* handleEditAttr(action) {
     const { templateId, attrId, attr, successCallback } = action.payload;
     yield call(TemplateAttr.editTemplateAttr, templateId, attrId, attr);
     if (successCallback) yield call(successCallback);
-    dispatchEvent(EVENT.GLOBAL_TOAST, { duration: 15000, i18nMessage: "editAttr", type: "success", });
-  } catch (e) {
     dispatchEvent(EVENT.GLOBAL_TOAST, {
       duration: 15000,
+      i18nMessage: 'editAttr',
+      type: 'success',
+    });
+  } catch (e) {
+    const i18nMessage = getErrorTranslation(e, 'editAttr', {
+      attrs_template_id_type_label_key: 'attrUnique',
+    });
+
+    dispatchEvent(EVENT.GLOBAL_TOAST, {
+      i18nMessage,
+      type: 'error',
+      duration: 15000,
       message: e.message,
-      i18nMessage: "editAttr",
-      type: "error",
     });
   } finally {
     yield put(loadingActions.removeLoading(constants.EDIT_ATTR));
