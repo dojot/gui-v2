6bcc42b09ebfa9e415db20a66b342fec10531c2f
LeonamMoura <leo99.music@gmail.com>
2022-08-19 11:09:21 -0300

    remove unused code

--- MFE/common/src/components/Layouts/MainLayout.jsx
+++ MFE/common/src/components/Layouts/MainLayout.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import { ContentContainer, RootContainer } from 'Components/Containers';
-import { Drawer } from 'Components/Drawer';
-
-import { MENU_ITEMS } from '../../menu';
 
 export default ({ isMenuOpen, children }) => {
   return (
     <RootContainer>
-      {/* <Drawer isOpen={isMenuOpen} menuItems={MENU_ITEMS}/> */}
       <ContentContainer isMenuOpen={isMenuOpen}>{children}</ContentContainer>
     </RootContainer>
   );
