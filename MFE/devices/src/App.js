0fc3940f40dd3c081b5b3f67963d43c80ba0ede2
LeonamMoura <leo99.music@gmail.com>
2022-09-28 13:17:50 -0300

    add route to createDevicesCSV

--- MFE/devices/src/App.js
+++ MFE/devices/src/App.js
@@ -4,6 +4,7 @@ import EditDevice from './view/editDevice';
 import Devices from './view/devices';
 import CreateDevice from './view/createDevice';
 import createMultipleDevices from './view/createMultipleDevices';
+import createDevicesCSV from './view/createDevicesCSV';
 import DeviceDetails from './view/deviceDetails';
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore';
@@ -18,9 +19,10 @@ export default ({ history }) => {
       <EventContainer i18n={i18n}>
         <Router history={history}>
           <Switch>
-            <Route path='/devices/edit/:deviceId' component={EditDevice} />
-            <Route path='/devices/new/multiple' component={createMultipleDevices} />
-            <Route path='/devices/new' component={CreateDevice} />
+            <Route path='/devices/edit/:deviceId' component={EditDevice} exact />
+            <Route path='/devices/new' component={CreateDevice} exact />
+            <Route path='/devices/new/multiple' component={createMultipleDevices} exact />
+            <Route path='/devices/new/csv' component={createDevicesCSV} exact />
             <Route path='/devices/:deviceId' component={DeviceDetails} exact />
             <Route path='/devices' component={Devices} exact />
           </Switch>
