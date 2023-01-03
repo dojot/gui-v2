cf63216f5d737e32206446b082bd435c2fa6b2d3
LeonamMoura <leo99.music@gmail.com>
2023-01-03 07:38:52 -0300

    add translation to simple table component head

--- MFE/dashboard/src/view/widget/table/Widget.jsx
+++ MFE/dashboard/src/view/widget/table/Widget.jsx
@@ -1,14 +1,24 @@
 import React, { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { WidgetCard, NoData } from 'sharedComponents/Cards';
 import { SimpleTable } from 'sharedComponents/Table';
 
 const TableWidget = ({ data, ...widgetProps }) => {
+  const { t } = useTranslation('dashboard');
   const { table } = widgetProps.config;
 
   const renderTable = useCallback(() => {
     if (data && data.length) {
-      return <SimpleTable columns={table} rows={data} hasTimestamp />;
+      return (
+        <SimpleTable
+          columns={table}
+          rows={data}
+          hasTimestamp
+          name={t('table.name')}
+          updatedAt={t('table.updatedAt')}
+        />
+      );
     }
     return <NoData />;
   }, [data, table]);
