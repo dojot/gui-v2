5100b079d9eb091ceec2bb85820b4a90e8a17c48
LeonamMoura <leo99.music@gmail.com>
2023-01-09 13:17:56 -0300

    add tooltip to menu icon button

--- MFE/common/src/components/Header/AppHeader.jsx
+++ MFE/common/src/components/Header/AppHeader.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-
-import { Toolbar, AppBar, IconButton, Typography } from '@material-ui/core';
+import { Toolbar, AppBar, IconButton, Typography, Tooltip } from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
-
 import PropTypes from 'prop-types';
-
+import { useTranslation } from 'react-i18next';
 import { useStyles } from './Style';
 
 const AppHeader = props => {
   const classes = useStyles();
+  const { t } = useTranslation('common');
+
   const { isOpen, handleClick, title, children } = props;
 
   return (
     <AppBar position='fixed' className={classes.appBar}>
       <Toolbar>
-        <IconButton
-          color='inherit'
-          aria-label='open drawer'
-          onClick={() => handleClick(!isOpen)}
-          edge='start'
-          data-testid='butonOpen'
-          className={classes.menuButton}
-        >
-          <Menu />
-        </IconButton>
+        <Tooltip title={isOpen ? t('closeMenu') : t('openMenu')} arrow>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={() => handleClick(!isOpen)}
+            edge='start'
+            data-testid='butonOpen'
+            className={classes.menuButton}
+          >
+            <Menu />
+          </IconButton>
+        </Tooltip>
 
         <Typography variant='h6' noWrap className={classes.title}>
           {title}
